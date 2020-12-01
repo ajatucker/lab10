4823c586194b300dea298c4c14152e28ce318318
AlexisTucker <ajatuck@umich.edu>
2020-12-01 12:25:19 -0500

    fixed selectionSort

--- selectionSort.java
+++ selectionSort.java
@@ -1,31 +1,33 @@
 
-public class selectionSort 
+public class selectionSort
 {
 	private int temp; /** Creates a new instance of SelectionSort */
-	public selectionSort() {    }    
-	/* A simple SelectionSort algorithm     
-	 * * pre-condition:      
-	 * * post-condition:     
-	 * * inputs:    
-	 *  * outputs:    
-	 *   * special conditions:    
-	 *    */    
-	public int[] basicSelectionSort(int[] x) 
-	{
-		for (int i = 0; i < x.length; ++i) 
-		{
-			for (int j= i+1; j < x.length; ++j) 
-			{
-				if (x[i] > x[j]) 
-				{                   
-					temp = x[i];                    
-					x[i] = x[j];                    
-					temp = x[j];                
-				}            
-			} // end of inner for loop        
-		} // end of outer for loop
-		return x;    
-		 // end of basicSelectionSort method}
-	
-	}
+	public selectionSort() {    }
+	/* A simple SelectionSort algorithm
+	 * * pre-condition:
+	 * * post-condition:
+	 * * inputs:
+	 *  * outputs:
+	 *   * special conditions:
+	 *    */
+	 public int[] basicSelectionSort(int[] x)
+ 	{
+ 		for (int i = 0; i < x.length-1; ++i)
+ 		{
+ 			int min = i;
+ 			for (int j= i+1; j < x.length; ++j)
+ 			{
+ 				if (x[j] < x[min])
+ 				{
+ 					min = j;
+ 				}
+ 			}
+ 			temp = x[min];
+ 			x[min] = x[i];
+ 			x[i] = temp;// end of inner for loop
+ 		} // end of outer for loop
+ 		return x;
+ 		 // end of basicSelectionSort method}
+ 	
+ 	}
 }
